4bd7b4c24a2e77b6ccaac56c3d98815de15f0d28
khoidm2004 <khoidm2004@gmail.com>
2023-10-26 16:37:30 +0300

    insert .env

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const route = require("./route");
+require('dotenv').config();
 
 app.use(express.json());
 
 app.use('/', route);
 
-const PORT = 3000;
-const HOST = "127.0.0.1";
+const PORT = process.env.PORT
+const HOST = process.env.HOST
 
 app.listen(PORT, HOST, () => {
     console.log(`Server is running on: http://${HOST}:${PORT}`);
