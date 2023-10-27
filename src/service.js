16f315c7de8d725e2f53f35f7c9137d44d89bd39
khoidm2004 <khoidm2004@gmail.com>
2023-10-27 16:27:07 +0300

    Debugging 1

--- src/service.js
+++ src/service.js
@@ -7,10 +7,10 @@ const PORT=parseInt(process.env.PORT, 10);
 const HOST = process.env.HOST
 const PROTOCOL=process.env.PROTOCOL !==undefined ? process.env.PROTOCOL:"http";
 const BASE_URL=`${PROTOCOL}://${HOST}:${PORT}`;
-const API_URL =`${BASE_URL}/api/v2`;
+const API_URL =`${BASE_URL}/API/v2`;
 
 app.get("/",(req,res)=>res.send("v2"));
-app.use('/api/v2',apiV2);
+app.use('/API/v2',apiV2);
 
 module.exports={
     app,
